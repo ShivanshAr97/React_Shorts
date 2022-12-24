1254c66bc2cff96eaa22e0242e2e4528506ba5f5
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2022-12-24 13:26:55 +0530

    commit

---
+++ shorts/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TimeOnClick from './components/TimeOnClick';
+
+function App() {
+  return(
+  <>
+  <TimeOnClick/>
+  </>
+  )
+}
+
+export default App;
---
+++ shorts/src/components/TimeOnClick.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react'
+
+const TimeOnClick = () => {
+    let d = new Date();
+    let time = d.toLocaleTimeString();
+    const [date, setDate] = useState(time);
+  
+    const TellTime=()=>{
+      d = new Date();
+      time = d.toLocaleTimeString();
+      setDate(time);
+      console.log("clicked "+time);
+    }
+    return (
+      <>
+      <h1>{date}</h1>
+      <button onClick={TellTime}>Click me</button>
+      </>
+    );
+}
+
+export default TimeOnClick
